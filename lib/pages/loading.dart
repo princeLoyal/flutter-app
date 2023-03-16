87686ae439dd233c9f324c6223b17d88f4fe6ab2
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-16 21:28:01 +0100

    Update loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
-  const Loading({Key? key}) : super(key: key);
-
+  
   @override
   State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+   Response response = await get('http://worldtimeapi.org/api/timezone/Africa/Lagos');
+   Map data = jsonDecode(response.body);
+   return AlertDialog(
+    title: Text('Succesfully received'),
+    content: Text('Api get recieved successfully')
+   );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    AlertDialog alert = getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: const Text('Loading'),
+      body: Column(
+children: <Widget>[
+alert,
+Text('Loading'),
+],
+      ),
     );
   }
 }
