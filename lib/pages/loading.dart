06884a46a716fdf2598830e4221a3895ac4d3ae3
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-03-17 13:18:20 +0100

    Update loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,19 +11,10 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getData() async{
-   var url =
-      Uri.https('www.googleapis.com', '/books/v1/volumes');
+   var url = Uri.https('https://worldtimeapi.org/api/timezone/Africa/Lagos');
 
   // Await the http get response, then decode the json-formatted response.
-  var response = await http.get(url);
-  if (response.statusCode == 200) {
-    var jsonResponse =
-        convert.jsonDecode(response.body) as Map<String, dynamic>;
-    var itemCount = jsonResponse['totalItems'];
-    print('Number of books about http: $itemCount.');
-  } else {
-    print('Request failed with status: ${response.statusCode}.');
-  }
+  
    
   }
 
